559fdab9e8e726492c47ae138fb23527264a197c
vandnanayak <94043284+vandnanayak@users.noreply.github.com>
2022-03-04 20:51:14 +0530

    Add files via upload
    Simple Intrest In C

---
+++ SIMPLE_I.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int p,r,t,si;
+clrscr();
+printf("Enter p,r,t");
+scanf("%d%d%d",&p,&r,&t);
+si=(p*r*t)/100;
+printf("intrest%d",si);
+getch();
+}
